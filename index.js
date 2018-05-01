42c2dfebe012f2430941e5ca77edefa5fd2224ac
david landau <thelandaus@gmail.com>
2018-05-01 01:18:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // write your code below!
+function happyHolidays(){
+  return "Happy Hollidays!";
+}
+function happyHolidaysTo(name){
+  return `Happy holidays, ${"Mom"}`;
+}
+function happyHolidaysTo(holiday,name){
+  return `Happy ${'Hannukah'}, ${'Mom'}`;
+}
