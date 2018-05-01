8c48789daff304b43a58e59664f2eded96cfe3a8
david landau <thelandaus@gmail.com>
2018-05-01 01:21:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ function happyHolidaysTo(name){
 function happyHolidaysTo(holiday,name){
   return `Happy ${'Hannukah'}, ${'Mom'}`;
 }
+function holidayCountdown(days,holidays){
+  return `It's ${19} days until ${"Shavuot"}!`
+}
